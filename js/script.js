ae735b9c1294595b9a9b056d341afc8ed03de3b3
yasmin-tomaz <88986854+yasmin-tomaz@users.noreply.github.com>
2021-10-07 16:43:46 +0000

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+document.getElementById("botaoEnviar").addEventListener("click", validaFormulario )
+
+function validaFormulario(){
+  if(document.getElementById("nome").value != "" &&
+     document.getElementById("email").value != "" &&
+     document.getElementById("telefone").value != "" ){
+     alert("Prontinho! Você receberá as novidades por email.")
+  }else{
+     alert("Por favor, preencha os campos de Nome e Email.")
+  }
+}
+
+
+
+
+
+
+  
